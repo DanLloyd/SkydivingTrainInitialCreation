86ec815e7723f76eba1cda05fe16adcb17a300fc
ChrisHodgetts <h006532c@student.staffs.ac.uk>
2014-11-06 21:01:20 +0000

    Yolo int
    Added the yolo int to test

---
+++ SkydivingTrainInitialCreation/AppMain.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+
+using Sce.PlayStation.Core;
+using Sce.PlayStation.Core.Environment;
+using Sce.PlayStation.Core.Graphics;
+using Sce.PlayStation.Core.Input;
+
+
+namespace SkydivingTrainInitialCreation
+{
+	public class AppMain
+	{
+		//GraphicsContext required for graphics, acts as a graphics 'instance'
+		private static GraphicsContext graphics;
+		private static Texture2D backgroundTexture;
+		private static Sprite background;
+		
+		
+		
+		public static void Main (string[] args)
+		{
+			Initialize();
+			
+			
+			//Game Loop
+			while (true) 
+			{
+				SystemEvents.CheckEvents();
+				Update ();
+				Render ();
+			}
+		}
+
+		public static void Initialize()
+		{
+			// Set up the graphics system
+			graphics = new GraphicsContext();
+			
+			//NOTE: Screen size for PSVITA = 960x544
+			backgroundTexture = new Texture2D("/Application/Assets/background.png", false);
+			
+			background = new Sprite(graphics, backgroundTexture);
+			background.Position.X = 0;
+			background.Position.Y = 0;
+
+		}
+
+		public static void Update()
+		{
+			// Query gamepad for current state
+			var gamePadData = GamePad.GetData (0);
+		}
+
+		public static void Render()
+		{
+			// Clear the screen
+			graphics.SetClearColor (0.0f, 0.0f, 0.0f, 0.0f);
+			graphics.Clear();
+			
+			//need to call Render() on each Sprite
+			background.Render();
+
+			// Present the screen
+			graphics.SwapBuffers();
+		}
+	}
+}
