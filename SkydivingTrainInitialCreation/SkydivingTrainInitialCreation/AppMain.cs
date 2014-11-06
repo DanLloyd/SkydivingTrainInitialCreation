327f4f65f23d70687d37f61fcf22dc76f411337f
DanLloyd <l014135c@student.staffs.ac.uk>
2014-11-06 20:37:23 +0000

    background completed
    Got a static 2D sprite on the screen to act as a background.

--- SkydivingTrainInitialCreation/SkydivingTrainInitialCreation/AppMain.cs
+++ SkydivingTrainInitialCreation/SkydivingTrainInitialCreation/AppMain.cs
@@ -6,43 +6,58 @@ using Sce.PlayStation.Core.Environment;
 using Sce.PlayStation.Core.Graphics;
 using Sce.PlayStation.Core.Input;
 
+
 namespace SkydivingTrainInitialCreation
 {
 	public class AppMain
 	{
 		private static GraphicsContext graphics;
+		private static Texture2D backgroundTexture;
+		private static Sprite background;
+		
+		
 		
 		public static void Main (string[] args)
 		{
-			Initialize ();
+			Initialize();
 
-			while (true) {
-				SystemEvents.CheckEvents ();
+			while (true) 
+			{
+				SystemEvents.CheckEvents();
 				Update ();
 				Render ();
 			}
 		}
 
-		public static void Initialize ()
+		public static void Initialize()
 		{
 			// Set up the graphics system
-			graphics = new GraphicsContext ();
+			graphics = new GraphicsContext();
+			
+			//NOTE: Screen size for PSVITA = 960x544
+			backgroundTexture = new Texture2D("/Application/Assets/background.png", false);
+			background = new Sprite(graphics, backgroundTexture);
+			background.Position.X = 0;
+			background.Position.Y = 0;
+
 		}
 
-		public static void Update ()
+		public static void Update()
 		{
 			// Query gamepad for current state
 			var gamePadData = GamePad.GetData (0);
 		}
 
-		public static void Render ()
+		public static void Render()
 		{
 			// Clear the screen
 			graphics.SetClearColor (0.0f, 0.0f, 0.0f, 0.0f);
-			graphics.Clear ();
+			graphics.Clear();
+			
+			background.Render();
 
 			// Present the screen
-			graphics.SwapBuffers ();
+			graphics.SwapBuffers();
 		}
 	}
 }
